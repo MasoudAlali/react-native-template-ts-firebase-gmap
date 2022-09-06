262da9617373dcfec80c0e62c0e32d207825e240
masoud <msdpcalali@gmail.com>
2022-09-06 21:16:53 +0430

    Init

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const {execSync} = require('child_process');
 
 console.log("Installing proper bundler version")
 execSync("gem install bundler:2.2.27");
-console.log('Installing Pods');
-execSync("npx pod-install");
+console.log("Installing gems");
+execSync("bungle install");
