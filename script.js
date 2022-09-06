15fa3163846938221ca6216a1ea2eae802377287
masoud <msdpcalali@gmail.com>
2022-09-06 21:18:08 +0430

    Init

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ const {execSync} = require('child_process');
 console.log("Installing proper bundler version")
 execSync("gem install bundler:2.2.27");
 console.log("Installing gems");
-execSync("bungle install");
+execSync("bundle install");
